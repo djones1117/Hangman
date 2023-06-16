9ebde0ebddbe6fecd5d48d2576154aba8d40b2b3
djones1117 <socjones21@yahoo.com>
2023-06-16 15:44:06 -0400

    Setup HTML & CSS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+  /*----- constants -----*/
+
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
