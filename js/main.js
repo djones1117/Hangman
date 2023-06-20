7ad2f913f9e506c68cd8d73c3cbc211330910624
djones1117 <socjones21@yahoo.com>
2023-06-20 09:11:51 -0400

    cached elements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
   /*----- constants -----*/
-
+const WORD = 'SEIROCKS';   // The word that needs to be guessed
+const MAX_GUESSES = 7;    //maximum number of guesses allowed.
 
   /*----- state variables -----*/
-
+let correctGuesses = [];   //array that stores correctly guessed letters
+let incorrectGuesses = []; //array that stores incorrectly guessed letters
+let guessesRemaining = MAX_GUESSES 
 
   /*----- cached elements  -----*/
-
-
+  const wordDisplay = document.getElementById('WordDisplay');
+  const keyboard = document.getElementById('Keyboard');
   /*----- event listeners -----*/
 
 
   /*----- functions -----*/
-
+  
