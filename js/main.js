df63640e4533894009f5f188705771e7289fa2b3
djones1117 <socjones21@yahoo.com>
2023-06-20 11:04:49 -0400

    functions pt 2

--- js/main.js
+++ js/main.js
@@ -14,8 +14,11 @@ let guessesRemaining = MAX_GUESSES
 
 
   /*----- functions -----*/
+  // Initialize the game
+initGame();
+
   
-// Function to initialize the game
+ // Function to initialize the game
 function initGame() {
     displayBlanks();
     keyboard.addEventListener('click', handleGuess);
@@ -31,3 +34,63 @@ function displayBlanks() {
       wordDisplay.appendChild(div);
     }
   }
+ 
+  // Function to handle the user's guess
+function handleGuess(event) {      
+    const letter = event.target.textContent;
+  //the function receives an event object as a parameter, representing the click event triggered by the player. 
+  //It extracts the letter that was clicked by accessing the textContent property of the clicked element. This assumes that each letter is enclosed within a <div> element in the keyboard section.
+  //notes for self when explaining function. 
+  //It checks if the extracted letter is either empty or not a valid alphabetic character using the isAlpha function. If it is empty or not a letter, the function returns and does not process the guess further.
+    if (!letter || !isAlpha(letter)) {
+      return;
+    }
+  //It checks if the letter has already been guessed correctly or incorrectly by checking if it exists in either the correctGuesses array or the incorrectGuesses array. If it has been guessed before, the function returns and does not process the guess further.
+    if (correctGuesses.includes(letter) || incorrectGuesses.includes(letter)) {
+      return;
+    }
+  //if the letter is a valid guess and has not been guessed before, it checks if it exists in the WORD string. If it does, it means the guess is correct. The letter is added to the correctGuesses array, and the updateWordDisplay function is called to update the displayed word with the correct guesses. Then, the checkWin function is called to check if the user has won the game.
+    if (WORD.includes(letter)) {
+      correctGuesses.push(letter);
+      updateWordDisplay();
+      checkWin();
+    } else {           
+      incorrectGuesses.push(letter);
+      updateGuessesRemaining();
+      checkLoss();
+    }
+  //If the letter is not in the WORD string, it means the guess is incorrect. The letter is added to the incorrectGuesses array, and the updateGuessesRemaining function is called to update the remaining number of guesses. Then, the checkLoss function is called to check if the user has lost the game.
+   
+  
+  event.target.classList.add('disabled');
+  }
+  //Finally, the clicked element's class is updated by adding the 'disabled' class. This class is used for styling purposes, typically to visually indicate that the letter has been guessed.
+  
+  // Function to check if the user has won
+function checkWin() {
+    const wordArray = Array.from(WORD);
+  
+    if (wordArray.every(letter => correctGuesses.includes(letter))) {
+      console.log('Congratulations! You have won the game!');
+    }
+  }
+
+
+  // Function to update the word display with correct guesses
+function updateWordDisplay() {
+    const blanks = wordDisplay.getElementsByClassName('Blank');
+  
+    for (let i = 0; i < WORD.length; i++) {
+      const letter = WORD[i];
+  
+      if (correctGuesses.includes(letter)) {
+        blanks[i].textContent = letter;
+      }
+    }
+  }
+  // Function to check if a character is a letter (A-Z or a-z)
+function isAlpha(ch) {
+    return /^[A-Z]$/i.test(ch);
+  }
+
+  
